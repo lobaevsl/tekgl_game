6ac8ca5208b9598d4d2df76d22f16760bf714fc7
lobaevsl <80401957+lobaevsl@users.noreply.github.com>
2023-09-17 08:40:09 +0000

    Deploying to gh-pages from @ lobaevsl/tekgl_game@f24ee5a671006116ed15ee2e44b74276b1677dd4 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1694938560|6317109";
+const CACHE_VERSION = "1694940001|4281277";
 const CACHE_PREFIX = "tekgl_game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
